0bb75b5ead5a39cb1c6a3a1594c0e96bd2360809
Stylianos Karagiannakis <stelioskaragiannakis2k@gmail.com>
2026-03-30 16:35:49 +0300

    text

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000' || 'https://server-1xgp.onrender.com';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000' || 'server-production-5868.up.railway.app';
